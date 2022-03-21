11e1baa7917ecfca05f85bf7d8ad091f97639c60
cicd <cicd>
2022-03-21 12:39:10 +0100

    vis queries

--- visualize_queries/models/query.py
+++ visualize_queries/models/query.py
@@ -5,6 +5,10 @@ from odoo.exceptions import UserError, RedirectWarning, ValidationError
 class Query(models.TransientModel):
     _name = 'db.query'
 
+    pid = fields.Integer("PID")
+    state = fields.Char("State")
+    started = fields.Datetime("Started")
+    usename = fields.Char("Username")
     name = fields.Char("Query")
     age = fields.Float("Age Seconds (Total)")
     age_minutes = fields.Float("Age Minutes (Total)", compute="_compute_age")
@@ -16,4 +20,25 @@ class Query(models.TransientModel):
             rec.age_hours = rec.age / 3600.0
 
     def cancel(self):
-        raise NotImplementedError("Cancel")
+        self.env.cr.execute((
+            "select pg_terminate_backend(%s)"
+        ), (self.id,))
+        self._update_queries()
+
+    @api.model
+    def _update_queries(self):
+        self.env.cr.execute((
+            "select pid, query_start, state, query, usename "
+            "from pg_stat_activity"
+        ))
+        pids = set()
+        for query in self.env.cr.dictfetchall():
+            query['name'] = query['query']
+            queries = self.search([('pid', '=', query['pid'])])
+            if not queries:
+                queries.create(query)
+            else:
+                queries.write(query)
+            pids.add(query['pid'])
+
+        self.search([('pid', 'not in', list(pids))]).unlink()
